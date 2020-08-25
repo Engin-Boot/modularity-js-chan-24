c04cb09f970d03531a6d2a04dc885e644c0f130d
Chandrashekar Arun Naik <canaik24@gmail.com>
2020-08-25 13:56:49 +0530

    Update getColorFromPairNumber.js

--- getColorFromPairNumber.js
+++ getColorFromPairNumber.js
@@ -1,13 +1,13 @@
-var MajorColors=require('./MajorColors.js');
-var MinorColors=require('./MinorColors.js');
+var majorColors=require('./majorColors.js');
+var minorColors=require('./minorColors.js');
 
 function getColorFromPairNumber(pairNumber) {
     let colorPair = {};
     const zeroBasedPairNumber = pairNumber - 1;
     const majorColorIndex =
-        Math.floor(zeroBasedPairNumber / MinorColors.length);
-    colorPair.major = MajorColors[majorColorIndex];
-    colorPair.minor = MinorColors[zeroBasedPairNumber % MinorColors.length];
+        Math.floor(zeroBasedPairNumber / minorColors.length);
+    colorPair.major = majorColors[majorColorIndex];
+    colorPair.minor = minorColors[zeroBasedPairNumber % minorColors.length];
     return colorPair
 }
 
